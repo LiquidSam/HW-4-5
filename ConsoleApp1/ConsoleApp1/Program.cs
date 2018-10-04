d63f3d23ed5e63e15396e7e6dca2a571094e3a5b
liquidsamlol <liquidsamlol@gmail.com>
2018-10-05 01:56:17 +0300

    deone

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the word  ");
+            string word = Convert.ToString(Console.ReadLine());
+
+            for (int i = 0; i < word.Length; i++)
+
+                Console.Write(word[word.Length - i - 1]);
+        }
+    }
+}
